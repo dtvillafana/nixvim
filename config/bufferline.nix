cfd0b18b895db8b4c2c1b0068d733fcb7bdf2e8d
dvillafana <david.villafana@capcu.org>
2024-10-04 15:01:33 -0500

    (fix) disable mouse in bufferline

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -6,6 +6,9 @@
                 options = {
                     buffer_close_icon = "";
                     close_icon = null;
+                    right_mouse_command = null;
+                    left_mouse_command = null;
+                    middle_mouse_command = null;
                 };
             };
         };
