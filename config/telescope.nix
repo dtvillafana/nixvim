3a4705faaf3afd859d0ac7fb0d91e5a5d9b87326
David Rog Desktop <dvillafanaiv@proton.me>
2024-11-18 19:36:56 -0600

    (fix) dedupe telescope bindings

--- config/telescope.nix
+++ config/telescope.nix
@@ -6,7 +6,6 @@
                 "<leader>ff" = "find_files";
                 "<leader>fg" = "live_grep";
                 "<leader>fh" = "help_tags";
-                "<leader>lfd" = "diagnostics";
                 "<leader>fs" = "lsp_document_symbols";
                 "<leader>fb" = "buffers";
                 "<leader>fk" = "keymaps";
