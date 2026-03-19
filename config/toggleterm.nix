880e94f736db8127e36fdccac3758fdba719d57e
David Villafaña <david.villafana@capcu.org>
2026-03-19 12:24:45 -0500

    feat: send visual selection to terminal (probably should implement sniprun at some point)

--- config/toggleterm.nix
+++ config/toggleterm.nix
@@ -1,5 +1,68 @@
 { pkgs, ... }:
 {
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "toggleterm-manager";
+      src = pkgs.fetchFromGitHub {
+        owner = "ryanmsnyder";
+        repo = "toggleterm-manager.nvim";
+        rev = "master";
+        hash = "sha256-7t61kcqeOS9hPXc9y88Sa8D0ZXIqxCXtxFQzmHKFJ8c=";
+      };
+      dependencies = [
+        pkgs.vimPlugins.telescope-nvim
+        pkgs.vimPlugins.plenary-nvim
+        pkgs.vimPlugins.toggleterm-nvim
+      ];
+    })
+  ];
+  extraConfigLua = ''
+    -- Pick a terminal via toggleterm-manager and send visual selection to it
+    function send_visual_to_picked_terminal()
+
+      -- Save the visual selection before opening the picker
+      local lines = vim.fn.getregion(
+        vim.fn.getpos("'<"),
+        vim.fn.getpos("'>"),
+        { type = vim.fn.visualmode() }
+      )
+
+      local function send_lines(prompt_bufnr, exit_on_action)
+        local actions_state = require("telescope.actions.state")
+        local entry = actions_state.get_selected_entry()
+        if entry then
+          local term = entry.value
+          for _, line in ipairs(lines) do
+            term:send(line, false)
+          end
+        end
+      end
+      require('toggleterm-manager').setup({
+        titles = {
+          preview = "Terminal Preview",
+          prompt = " Terminals",
+          results = "Results",
+        },
+        mappings = {
+          i = {
+            ["<CR>"] = {
+              action = send_lines,
+              exit_on_action = true,
+            },
+          },
+          n = {
+            ["<CR>"] = {
+              action = send_lines,
+              exit_on_action = true,
+            },
+          },
+        },
+      })
+      require('toggleterm-manager').open({
+        titles = { prompt = " Send to Terminal" },
+      })
+    end
+  '';
   plugins = {
     toggleterm = {
       enable = true;
@@ -38,7 +101,7 @@
     {
       mode = "n";
       key = "<leader>tf";
-      action = ''<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'float', id = vim.v.count, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>'';
+      action = "<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'float', id = vim.v.count, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
@@ -47,7 +110,7 @@
     {
       mode = "n";
       key = "<leader>tv";
-      action = ''<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'vertical', id = vim.v.count + 10, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>'';
+      action = "<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'vertical', id = vim.v.count + 10, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
@@ -56,7 +119,7 @@
     {
       mode = "n";
       key = "<leader>th";
-      action = ''<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'horizontal', id = vim.v.count + 20, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>'';
+      action = "<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'horizontal', id = vim.v.count + 20, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
@@ -65,7 +128,7 @@
     {
       mode = "n";
       key = "<leader>tt";
-      action = ''<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'tab', id = vim.v.count + 30, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>'';
+      action = "<CMD>lua require('toggleterm.terminal').Terminal:new({ hidden = true, direction = 'tab', id = vim.v.count + 30, display_name = 'Terminal ' .. vim.v.count, }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
@@ -74,7 +137,7 @@
     {
       mode = "n";
       key = "<leader>tl";
-      action = ''<CMD>lua local current_buf = vim.api.nvim_get_current_buf(); local filepath = vim.api.nvim_buf_get_name(current_buf); local directory = vim.fn.fnamemodify(filepath, ':h'); require('toggleterm.terminal').Terminal:new({ cmd = 'lazygit', hidden = true, close_on_exit = true, direction = 'float', dir = directory, display_name = 'lazygit', }):toggle()<CR>'';
+      action = "<CMD>lua local current_buf = vim.api.nvim_get_current_buf(); local filepath = vim.api.nvim_buf_get_name(current_buf); local directory = vim.fn.fnamemodify(filepath, ':h'); require('toggleterm.terminal').Terminal:new({ cmd = 'lazygit', hidden = true, close_on_exit = true, direction = 'float', dir = directory, display_name = 'lazygit', }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
@@ -83,7 +146,7 @@
     {
       mode = "n";
       key = "<leader>tb";
-      action = ''<CMD>lua require('toggleterm.terminal').Terminal:new({ cmd = 'btop', hidden = true, close_on_exit = true, direction = 'float', display_name = 'btop' }):toggle()<CR>'';
+      action = "<CMD>lua require('toggleterm.terminal').Terminal:new({ cmd = 'btop', hidden = true, close_on_exit = true, direction = 'float', display_name = 'btop' }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
@@ -91,48 +154,28 @@
     }
     {
       mode = "x";
-      key = "<leader>tsf";
-      action = '':<C-u>lua require('toggleterm').send_lines_to_terminal("visual_selection", true, { args = tostring(vim.v.count) })<CR>'';
+      key = "<leader>ts";
+      action = ":<C-u>lua send_visual_to_picked_terminal()<CR>";
       options = {
         noremap = true;
         silent = true;
-        desc = "Send visual selection to float terminal [count]";
+        desc = "Send visual selection to a terminal (picker)";
       };
     }
     {
-      mode = "x";
-      key = "<leader>tsv";
-      action = '':<C-u>lua require('toggleterm').send_lines_to_terminal("visual_selection", true, { args = tostring(vim.v.count + 10) })<CR>'';
+      mode = "n";
+      key = "<leader>tp";
+      action = "<CMD>Telescope toggleterm_manager<CR>";
       options = {
         noremap = true;
         silent = true;
-        desc = "Send visual selection to vertical terminal [count]";
-      };
-    }
-    {
-      mode = "x";
-      key = "<leader>tsh";
-      action = '':<C-u>lua require('toggleterm').send_lines_to_terminal("visual_selection", true, { args = tostring(vim.v.count + 20) })<CR>'';
-      options = {
-        noremap = true;
-        silent = true;
-        desc = "Send visual selection to horizontal terminal [count]";
-      };
-    }
-    {
-      mode = "x";
-      key = "<leader>tst";
-      action = '':<C-u>lua require('toggleterm').send_lines_to_terminal("visual_selection", true, { args = tostring(vim.v.count + 30) })<CR>'';
-      options = {
-        noremap = true;
-        silent = true;
-        desc = "Send visual selection to tab terminal [count]";
+        desc = "Toggle terminal picker";
       };
     }
     {
       mode = "n";
       key = "<leader>tj";
-      action = ''<CMD>lua require('toggleterm.terminal').Terminal:new({ cmd = '${pkgs.nodejs}/bin/node', hidden = true, close_on_exit = true, direction = 'float', display_name = 'nodejs' }):toggle()<CR>'';
+      action = "<CMD>lua require('toggleterm.terminal').Terminal:new({ cmd = '${pkgs.nodejs}/bin/node', hidden = true, close_on_exit = true, direction = 'float', display_name = 'nodejs' }):toggle()<CR>";
       options = {
         noremap = true;
         silent = true;
