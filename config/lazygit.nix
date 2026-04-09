5fcadd89a798bada1ff81938a6cbe23aa888bc81
dtvillafana <82293276+dtvillafana@users.noreply.github.com>
2026-04-09 14:25:28 -0500

    fix: diff-so-fancy (yolo)

--- config/lazygit.nix
+++ config/lazygit.nix
@@ -1,9 +1,21 @@
 { pkgs, lib, ... }:
 let
+  diffSoFancyWrapper = pkgs.writeShellScript "diff-so-fancy-wrapper" ''
+    export GIT_CONFIG_COUNT=4
+    export GIT_CONFIG_KEY_0="color.diff-highlight.newnormal"
+    export GIT_CONFIG_VALUE_0="green"
+    export GIT_CONFIG_KEY_1="color.diff-highlight.newhighlight"
+    export GIT_CONFIG_VALUE_1="bold green"
+    export GIT_CONFIG_KEY_2="color.diff-highlight.oldnormal"
+    export GIT_CONFIG_VALUE_2="red"
+    export GIT_CONFIG_KEY_3="color.diff-highlight.oldhighlight"
+    export GIT_CONFIG_VALUE_3="bold red"
+    exec ${lib.getExe pkgs.diff-so-fancy} "$@"
+  '';
   lazygitConfig = pkgs.writeText "lazygit-config.yml" ''
     git:
       pagers:
-        - pager:  ${lib.getExe pkgs.diff-so-fancy}
+        - pager:  ${diffSoFancyWrapper}
           colorArg: always
           useConfig: false
   '';
