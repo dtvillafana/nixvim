44408d3326c70d06b86a7af27df6fc0bdba4ffad
David Villafaña <david.villafana@capcu.org>
2026-05-20 17:16:40 -0500

    feat: add neoclip

--- config/telescope.nix
+++ config/telescope.nix
@@ -14,6 +14,7 @@
         "<leader>fq" = "quickfix";
         "<leader>fx" = "manix";
         "<leader>fc" = "command_history";
+        "<leader>fp" = "neoclip";
         "<leader>/" = "current_buffer_fuzzy_find";
       };
       settings = {
@@ -115,14 +116,81 @@
   };
   extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
-      name = "tmux-switch";
+      name = "nvim-neoclip";
       src = pkgs.fetchFromGitHub {
-        owner = "jkeresman01";
-        repo = "tmux-switch.nvim";
-        rev = "main";
-        hash = "sha256-GIKsuHnt26vM+/BRsX0d2aHVEOg1PIVNmj/U+UxZqi8=";
+        owner = "AckslD";
+        repo = "nvim-neoclip.lua";
+        rev = "master";
+        hash = "sha256-IjkpqLHGO48QRiYkELyadZK2uFXQVyklGfBJSgAbzIY=";
       };
       doCheck = false;
     })
   ];
+  extraConfigLua = ''
+    require('neoclip').setup({
+      history = 1000,
+      enable_persistent_history = false,
+      length_limit = 1048576,
+      continuous_sync = false,
+      db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
+      filter = nil,
+      preview = true,
+      prompt = nil,
+      default_register = '"',
+      default_register_macros = 'q',
+      enable_macro_history = true,
+      content_spec_column = false,
+      disable_keycodes_parsing = false,
+      dedent_picker_display = false,
+      initial_mode = 'insert',
+      on_select = {
+    	move_to_front = false,
+    	close_telescope = true,
+      },
+      on_paste = {
+    	set_reg = false,
+    	move_to_front = false,
+    	close_telescope = true,
+      },
+      on_replay = {
+    	set_reg = false,
+    	move_to_front = false,
+    	close_telescope = true,
+      },
+      on_custom_action = {
+    	close_telescope = true,
+      },
+      keys = {
+    	telescope = {
+    	  i = {
+    		select = '<cr>',
+    		paste = '<c-p>',
+    		paste_behind = '<c-k>',
+    		replay = '<c-q>',  -- replay a macro
+    		delete = '<c-d>',  -- delete an entry
+    		edit = '<c-e>',  -- edit an entry
+    		custom = {},
+    	  },
+    	  n = {
+    		select = '<cr>',
+    		paste = 'p',
+    		--- It is possible to map to more than one key.
+    		-- paste = { 'p', '<c-p>' },
+    		paste_behind = 'P',
+    		replay = 'q',
+    		delete = 'd',
+    		edit = 'e',
+    		custom = {},
+    	  },
+    	},
+    	fzf = {
+    	  select = 'default',
+    	  paste = 'ctrl-p',
+    	  paste_behind = 'ctrl-k',
+    	  custom = {},
+    	},
+      },
+    })
+
+  '';
 }
